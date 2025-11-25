e76b430fc27bb9ae5a041f6b583a060a93faa2e9
Bittu_Sharma <procoder192@gmail.com>
2025-11-26 02:03:53 +0530

    add Loader component

---
+++ frontend/src/component/Loader.jsx
@@ -0,0 +1,8 @@
+export default function Loader() {
+    return (
+      <div className="flex justify-center py-5">
+        <div className="w-10 h-10 border-4 border-gray-300 border-t-black rounded-full animate-spin"></div>
+      </div>
+    );
+  }
+  
